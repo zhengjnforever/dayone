8c776f246c7faf3a39553443d3234410b17e946f
Sam <1669360420@qq.com>
2020-06-16 15:17:16 +0800

    本地工程也能够添加到文件

---
+++ git-test-project/src/com/git/FirstClass.java
@@ -0,0 +1,14 @@
+package com.git;
+
+/**
+ * @program: git-test-project
+ * @description: d
+ * @author: Sam
+ * @create: 2020-06-16 15:12
+ **/
+
+public class FirstClass {
+    public static void main(String[] args){
+        System.out.println("hello git");
+    }
+}

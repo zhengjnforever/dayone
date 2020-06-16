a5d8b9f006506eea26d2e5ee305cd67a3388db5b
Sam <1669360420@qq.com>
2020-06-16 15:19:55 +0800

    添加test方法

--- git-test-project/src/com/git/FirstClass.java
+++ git-test-project/src/com/git/FirstClass.java
@@ -11,4 +11,7 @@ public class FirstClass {
     public static void main(String[] args){
         System.out.println("hello git");
     }
+    public void test(){
+        System.out.println();
+    }
 }

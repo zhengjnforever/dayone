6ae23d7f8a48abf687f7c239d90e7cccf31fe079
Sam <1669360420@qq.com>
2020-07-13 10:00:31 +0800

    创建的springboot基础

---
+++ dayone/src/main/java/com/example/dayone/DayoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.dayone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DayoneApplication.class, args);
+    }
+
+}
---
+++ dayone/src/test/java/com/example/dayone/DayoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dayone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
